556f0437d03429ff974616fc0d73d258ec51b2be
marcelo-martins <marcelobmartins219@hotmail.com>
2020-02-19 14:13:11 -0300

    getting new versions

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ status=$(curl -s --head -w %{http_code} https://oplab9.parqtec.unicamp.br/pub/pp
 addr="ftp://oplab9.parqtec.unicamp.br"
 path="/test/marcelo/rclone/latest/"
 
-   
 if [ [$status==404] ]
 then
     echo "CLONING"
@@ -33,12 +32,12 @@ then
     ls -la
     
     echo "MOVING BINARY"
-    # if [[ $github_version > $ftp_version ]]
-    # then
-    #     #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /test/marcelo/rclone/latest rclone-$github_version"
-    #     #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/rclone/latest/rclone-$ftp_version"
-    # fi
-    lftp -c "open -u $FTP_USER,$FTP_PASSWORD ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/rclone rclone-$github_version"
+    if [[ $github_version > $ftp_version ]]
+    then
+        lftp -c "open -u $FTP_USER,$FTP_PASSWORD ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/rclone rclone-$github_version"
+        #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/rclone/latest/rclone-$ftp_version"
+    fi
+    # lftp -c "open -u $FTP_USER,$FTP_PASSWORD ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/rclone rclone-$github_version"
     #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/rclone-$del_version"
 
     # lftp -c "open -u $USER,$PASS $addr; cd $path; mv rclone-$ftp_version ../"
