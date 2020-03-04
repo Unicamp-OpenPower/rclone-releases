bdd2f260d7983199cf43f2ddf61db2a7813d6f3b
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-03-03 22:12:52 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 github_version=$(cat github_version.txt)
 ftp_version=$(cat ftp_version.txt)
-del_version=$(cat delete_version.txt)
+#del_version=$(cat delete_version.txt)
 
 addr="ftp://oplab9.parqtec.unicamp.br"
 echo $github_version
@@ -15,20 +15,9 @@ then
     go get github.com/rclone/rclone/backend/all github.com/rclone/rclone/cmd github.com/rclone/rclone/cmd/all github.com/rclone/rclone/lib/plugin
     go build
     mv rclone rclone-$github_version
-    ls -a
+    #ls -a
     ./rclone-$github_version --help
     
-
-    #if [[ $github_version > $ftp_version ]]
-    #then
-        #lftp -c "open -u $FTP_USER,$FTP_PASSWORD ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/rclone rclone-$github_version"
-        #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /test/marcelo/rclone/latest/rclone-$ftp_version"
-    #fi
-    # lftp -c "open -u $FTP_USER,$FTP_PASSWORD ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/rclone rclone-$github_version"
+    lftp -c "open -u $FTP_USER,$FTP_PASSWORD ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/rclone rclone-$github_version"
     #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/rclone-$del_version"
-
-    # lftp -c "open -u $USER,$PASS $addr; cd $path; mv rclone-$ftp_version ../"
-
-    # mv rclone rclone-$github_version
-    # lftp -c "open -u $USER,$PASS $addr; cd $path; put rclone-$github_version"
 fi
