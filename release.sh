f7882d38b463913d7157b113c74ad47356fabaef
juliokiyoshi <38574345+juliokiyoshi@users.noreply.github.com>
2020-09-13 10:40:37 -0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -17,8 +17,8 @@ if [ "$github_version" != "$ftp_version" ]
     sudo mv empacotar-deb.sh  $LOCALPATH
     sudo mv empacotar-rpm.sh  $LOCALPATH
     cd  $LOCALPATH
-    sudo ./empacotar-deb.sh rclone rclone-$github_version2 $github_version2 " "
-    sudo ./empacotar-rpm.sh rclone rclone-$github_version2 $github_version2 " " "Rclone ("rsync for cloud storage") is a command line program to sync files and directories to and from different cloud storage providers."
+    sudo ./empacotar-deb.sh rclone rclone-$github_version $github_version2 " "
+    sudo ./empacotar-rpm.sh rclone rclone-$github_version $github_version2 " " "Rclone ("rsync for cloud storage") is a command line program to sync files and directories to and from different cloud storage providers."
 fi
 
 if [[ $github_version != $ftp_version ]]
